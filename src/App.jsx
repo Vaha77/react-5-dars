af0e001fe60d5d43b6f9a2647926a457a5381b94
vaha77 <abdulvohid996@gmail.com>
2021-08-12 17:20:22 +0900

    add delte sort

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./style.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      status: "",
+      data: [
+        { id: 1, name: "Jabbor", status: "Medium" },
+        { id: 2, name: "Mansur", status: "High" },
+        { id: 3, name: "Jumanazar", status: "Low" },
+        { id: 4, name: "Abdulvohid", status: "Vaha" },
+      ],
+    };
+  }
+
+  render() {
+    const onDelete = (id) => {
+      console.log("delete", id);
+      const newData = this.state.data.filter((value) => value.id !== id);
+      this.setState({ data: newData });
+    };
+    const onAdd = () => {
+      console.log(this.state.name, this.state.status);
+      const newData = [
+        ...this.state.data,
+        {
+          id: this.state.data.length + 1,
+          name: this.state.name,
+          status: this.state.status,
+        },
+      ];
+      this.setState({ data: newData });
+    };
+    const onChangeName = (e) => {
+      this.setState({ name: e.target.value });
+    };
+    const onChangeSatatus = (e) => {
+      this.setState({ status: e.target.value });
+    };
+    return (
+      <div>
+        <input onChange={onChangeName} placeholder="name" type="text" />
+        <input onChange={onChangeSatatus} placeholder="status" type="text" />
+        <button onClick={onAdd}>add</button>
+        <table border="1" style={{ borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((value) => {
+              return (
+                <tr key={value.id}>
+                  <td>{value.id}</td>
+                  <td> {value.name} </td>
+                  <td> {value.status} </td>
+                  <td>
+                    <button onClick={() => onDelete(value.id)}>delete</button>
+                    <button onClick={() => onDelete(value.id)}>Add</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <hr />
+      </div>
+    );
+  }
+}
